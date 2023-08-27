eecf820ce9d12c244a45fc9a6d567f3a6e85f7d7
kanchihari <harishkanchi.rishi@gmail.com>
2023-08-27 23:29:19 +0200

    Initialize quiz setup and question, fetched questions using a api url and logged quiz questions from API

--- javascript.js
+++ javascript.js
@@ -11,3 +11,37 @@ const progress = (value) => {
 
 };
 
+let questions = [],
+    time = 30,
+    score = 0,
+    currentQuestion,
+    timer;
+
+    //targeting the elements in the homepage
+const startButton = document.querySelector(".start"),
+numQuestions= document.querySelector("#num-questions"),
+category = document.querySelector("#category"),
+difficultyLevel = document.querySelector("#difficulty-level"),
+timePerQuestion = document.querySelector("#time"),
+quiz = document.querySelector(".quiz"),
+homepage = document.querySelector(".homepage");
+
+const startQuiz = () => {
+    const num =numQuestions.value;
+    cat = category.value;
+    diff = difficultyLevel.value;
+
+
+    // Api url for generating questions.
+    const url = `https://opentdb.com/api.php?amount=${num}&category=${cat}&difficultyLevel=${diff}&type=multiple`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((date) => {
+            questions = data.results;
+            console.log(questions);
+        });
+};
+
+
+
