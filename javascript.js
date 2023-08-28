cefbc0d94d73c737acea247b9069053e79b7dc3b
kanchihari <harishkanchi.rishi@gmail.com>
2023-08-28 14:39:51 +0200

    fetched questions are stored in questions array,display them in qestionText and hompage is hidden

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 const progressBar = document.querySelector(".progress-bar"),
 progressText = document.querySelector(".progress-text");
 
@@ -7,7 +9,7 @@ const progress = (value) => {
      */
     const percentage = (value / time) * 100;
     progressBar.style.width = `${percentage}% `;
-    progressText.innerHTML = ` ${value} `;
+    progressText.innerHTML = `${value}`;
 
 };
 
@@ -18,7 +20,7 @@ let questions = [],
     timer;
 
     //targeting the elements in the homepage
-const startButton = document.querySelector(".start"),
+const startButton = document.querySelector(".btn-start"),
 numQuestions= document.querySelector("#num-questions"),
 category = document.querySelector("#category"),
 difficultyLevel = document.querySelector("#difficulty-level"),
@@ -27,21 +29,40 @@ quiz = document.querySelector(".quiz"),
 homepage = document.querySelector(".homepage");
 
 const startQuiz = () => {
-    const num =numQuestions.value;
+    const num = numQuestions.value;
     cat = category.value;
     diff = difficultyLevel.value;
 
 
     // Api url for generating questions.
+
     const url = `https://opentdb.com/api.php?amount=${num}&category=${cat}&difficultyLevel=${diff}&type=multiple`;
+    /**URL for fetching questions from an API.  
+     * includes query parameters that define the number of questions to fetch (num), the category of questions (cat), 
+     * the difficulty level of questions (diff), and the type of questions (multiple-choice in this case). */
+
 
     fetch(url)
         .then((res) => res.json())
-        .then((date) => {
-            questions = data.results;
+        .then((data) => {
+            // Code to handle the fetched data
+            questions = data.results;  //  manipulate the fetched questions data
             console.log(questions);
+            homepage.classList.add("hide");
+            quiz.classList.remove("hide");
+            currentQuestion = 1;
+            showQuestion(questions[0]);
         });
 };
 
+startButton.addEventListener("click", startQuiz);
 
+const showQuestion = (question) => {
+    const questionText = document.querySelector(".question"),
+    answersWrapper = document.querySelector(".answer-wrapper"),
+    questionNumber = document.querySelector(".question-number");
 
+    questionText.innerHTML =question.question;
+};
+
+});
